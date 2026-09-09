29356c850443aabde7a67a907b81d4d63474e63a
Shivam Joshi <shivamj0002@gmail.com>
2026-09-10 01:13:47 +0530

    Time: 12 ms (1.12%) | Memory: 19.3 MB (60.39%) - LeetSync

--- 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.py
+++ 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.py
@@ -6,13 +6,10 @@ class Solution:
                 if nums[j]<nums[small]:
                     small=j
             nums[i],nums[small]=nums[small],nums[i]
-        print(nums)
         hash={}
         for i in nums:
             hash[i]=1
-        print(hash)
         for i in range(k,nums[-1]+k+1):
-            print(f"Current i {i}")
             if i not in hash:
                 if i%k==0:
                     return i
